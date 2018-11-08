ffe74a78af202ae4e261b2775aafd7891fa1851b
Kenneth Edwards <kennethedwards@Kenneths-MBP.fritz.box>
2018-11-08 07:28:55 +0100

    intital upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatSidenavModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidenavComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild } from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent {
+  // opened = true;
+  // events = [];
+
+  @ViewChild('sidenav') sidenav: MatSidenav;
+
+  reason = '';
+
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
+  }
+
+
+}
